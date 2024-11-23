bcad313a78d071d52678628f818753a760bae01d
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-11-23 17:09:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 function makeid(l) {
-  // write your code here
+
+	let str='qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890';
+
+	let newstr="";
+	for(let i=0;i<l;i++)
+		{
+			let x=Math.floor(Math.random()*61)+1;
+
+			let ch=str.charAt(x);
+			newstr+=ch;
+		}
+
+	return newstr;
+	
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+// const l = prompt("Enter a number.");
+// alert(makeid(l));
